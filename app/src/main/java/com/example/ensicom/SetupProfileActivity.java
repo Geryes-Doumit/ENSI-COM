ada0a5723ce4c4a81079428973d592e18616ee77
Peter <lin.peter@outlook.fr>
2023-06-06 10:34:59 +0200

    peter ensicom login+register

---
+++ app/src/main/java/com/example/ensicom/SetupProfileActivity.java
@@ -0,0 +1,15 @@
+package com.example.ensicom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class SetupProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup_profile);
+    }
+}
