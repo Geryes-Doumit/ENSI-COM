7e1e918ccd264850e13a57b73fe62ecf85229ed0
Peter <lin.peter@outlook.fr>
2023-06-08 14:33:13 +0200

    post deletion

--- app/src/main/java/com/example/ensicom/UserAndPostRecyclerAdapter.java
+++ app/src/main/java/com/example/ensicom/UserAndPostRecyclerAdapter.java
@@ -1,5 +1,7 @@
 package com.example.ensicom;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +11,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -37,6 +40,7 @@ public class UserAndPostRecyclerAdapter extends RecyclerView.Adapter<ItemViewHol
     @Override
     public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
         String currentUserUid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        int currentPosition = position;
         ClassicPost post = postsList.get(position);
 
         String postContent = post.getContent();
@@ -52,17 +56,55 @@ public class UserAndPostRecyclerAdapter extends RecyclerView.Adapter<ItemViewHol
                 User postUser = dataSnapshot.getValue(User.class);
                 String postUserName = postUser.getUsername();
                 String profilePictureUrl = postUser.getProfilePicture();
+                String postId = post.getPostId();
                 if (dataSnapshot.getKey().equals(currentUserUid)) {
                     holder.getDeletePostButton().setVisibility(View.VISIBLE);
                 }
                 else {
                     holder.getDeletePostButton().setVisibility(View.GONE);
                 }
+                holder.getDeletePostButton().setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        new AlertDialog.Builder(v.getContext()).setTitle("Supprimer le post")
+                                .setMessage("Êtes-vous sûr de vouloir supprimer ce post ?")
+                                .setPositiveButton(android.R.string.yes,null)
+                                .setPositiveButton("Oui", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        deletePost(postId);
+                                        Toast.makeText(v.getContext(), "Post supprimé", Toast.LENGTH_SHORT).show();
+                                        postsList.remove(currentPosition);
+                                        notifyDataSetChanged();
+                                       }
 
+                                })
+                                .setNegativeButton(android.R.string.no, null)
+                                .setNegativeButton("Non", null)
+                                .setIcon(android.R.drawable.ic_dialog_alert)
+                                .show();
+                    }
+                });
                 holder.getUserName().setText(postUserName);
                 holder.getPostContent().setText(postContent);
                 Glide.with(holder.getUserProfilePicture().getContext()).load(profilePictureUrl).into(holder.getUserProfilePicture());
                 Glide.with(holder.getPostPicture1().getContext()).load(postPicture1).into(holder.getPostPicture1());
+            }
+        });
+    }
+    public void deletePost(String postId) {
+        DatabaseReference postRef = FirebaseDatabase
+                .getInstance("https://projet-fin-annee-ddbef-default-rtdb.europe-west1.firebasedatabase.app/")
+                .getReference("posts")
+                .child(postId);
+        postRef.removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
             }
         });
     }
