50ad278a93e225bb306c8cc637ce8c5dc022f71a
Anthony <anthony_bnt@outlook.com>
2023-06-12 20:11:23 +0200

    calendar merge with main project

---
+++ app/src/main/java/com/example/ensicom/PresentationActivity.java
@@ -0,0 +1,64 @@
+package com.example.ensicom;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PresentationActivity extends AppCompatActivity {
+
+    private static final long DELAY_MILLIS = 1500; // Délai en millisecondes
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_presentation);
+
+        ImageView logoImage = findViewById(R.id.logo_image);
+        TextView welcomeMessage = findViewById(R.id.welcome_message);
+
+        // Animation d'apparition du logo et du message
+        AlphaAnimation fadeInAnimation = new AlphaAnimation(0, 1);
+        fadeInAnimation.setDuration(2000); // Durée de l'animation en millisecondes
+
+        logoImage.startAnimation(fadeInAnimation);
+        welcomeMessage.startAnimation(fadeInAnimation);
+
+        // Rend le logo et le message visibles à la fin de l'animation
+        fadeInAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {}
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                logoImage.setVisibility(View.VISIBLE);
+                welcomeMessage.setVisibility(View.VISIBLE);
+
+                // Programmation de la tâche différée pour masquer le logo et lancer l'activité par défaut
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        logoImage.setVisibility(View.INVISIBLE);
+                        welcomeMessage.setVisibility(View.INVISIBLE);
+                        launchDefaultActivity();
+                    }
+                }, DELAY_MILLIS);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {}
+        });
+    }
+
+    private void launchDefaultActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish(); // Termine l'activité de présentation pour revenir à l'activité par défaut
+    }
+}
