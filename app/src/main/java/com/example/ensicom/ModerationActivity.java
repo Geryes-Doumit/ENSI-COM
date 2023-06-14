0606513a5cf23198385082128471ca0023e68cb6
Peter <lin.peter@outlook.fr>
2023-06-14 17:55:07 +0200

    toast modération

--- app/src/main/java/com/example/ensicom/ModerationActivity.java
+++ app/src/main/java/com/example/ensicom/ModerationActivity.java
@@ -10,6 +10,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.DataSnapshot;
@@ -39,6 +40,9 @@ public class ModerationActivity extends AppCompatActivity {
         swipeRefreshLayout.setOnRefreshListener(() -> {
             getPosts();
             swipeRefreshLayout.setRefreshing(false);
+            if (postsList.size()==0) {
+                Toast.makeText(ModerationActivity.this, "Aucun post à modérer", Toast.LENGTH_SHORT).show();
+            }
         });
     }
     public void getPosts() {
