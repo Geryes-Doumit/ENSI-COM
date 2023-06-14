48976522ee7cfc5de0888069b0a2541b0f70345e
TinySharky <anthony_bnt@outlook.com>
2023-06-14 11:46:10 +0200

    event design + comment design + comment image gone

--- app/src/main/java/com/example/ensicom/AboutActivity.java
+++ app/src/main/java/com/example/ensicom/AboutActivity.java
@@ -3,19 +3,11 @@ package com.example.ensicom;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.view.View;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.WindowCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 import com.example.ensicom.databinding.ActivityAboutBinding;
 
